52216241bb534ed74bd0c7dea832f36006682832
Vladislav Altanov <cactusabg@gmail.com>
2016-03-12 12:56:50 +0000

    Created AtoZ page

---
+++ src/AppBundle/Controller/AtoZController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AtoZController extends Controller
+{
+    /**
+     * @Route("/a-z/{letter}", name="A to Z listing")
+     */
+    public function indexAction(Request $request, $letter)
+    {
+        // echo '<pre>';
+        // print_r($letter);
+        // echo '<pre>';
+        // replace this example code with whatever you need
+        return $this->render('atoz/index.html.twig', array(
+            'testing' => 'tested',
+            'letter' => $letter
+        ));
+    }
+}
